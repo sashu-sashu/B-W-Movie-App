709f0073496a42616a4c4cc7f5e58210111efd94
sashu-sashu <iamsashaalexandra@gmail.com>
2022-07-25 13:12:27 +0200

    Express get function added

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
-console.log('hi sasha')
+const express = require('express');
+const app = express();
+
+let topMovies = [
+    {
+      title: 'Coffee and Cigarettes',
+      director: 'Jim Jarmusch',
+      year: '2003',
+      duration: '96 min'
+    },
+    {
+      title: 'Schindler/’s List',
+      director: 'J.R.R. Tolkien',
+      year: '1993',
+      duration: '195 min'
+    },
+    {
+      title: 'Metropolis',
+      director: 'Fritz Lang',
+      year: '1926',
+      duration: '109 min'
+    },
+    {
+      title: 'Psycho',
+      director: 'Alfred Hitchcock',
+      year: '1960',
+      duration: '109 min'
+    },
+    {
+      title: 'To Kill A Mockingbird',
+      director: 'Robert Mulligan',
+      year: '1962',
+      duration: '129 min'
+    },
+    {
+      title: 'All About Eve',
+      director: 'Joseph L. Mankiewicz',
+      year: '1950',
+      duration: '138 min'
+    },
+    {
+      title: 'Casablanca',
+      director: 'Starring Humphrey Bogart and Ingrid Bergman',
+      year: '1942',
+      duration: '102 min'
+    },
+    {
+      title: 'The Seventh Seal',
+      director: 'Ingmar Bergman',
+      year: '1957',
+      duration: '2 h'
+    },
+    {
+      title: 'Modern Times',
+      director: 'Charles Chaplin',
+      year: '1936',
+      duration: '87 min'
+    },
+    {
+      title: '8½',
+      director: 'Federico Fellini',
+      year: '1963',
+      duration: '138 min'
+    },
+    
+
+  ];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie club!');
+});
+
+app.get('/movies', (req, res) => {                  
+  res.sendFile('public/movies.html', { root: __dirname });
+});
+
+// function for static files
+// this syntax does not
+//app.use('/documentation', express.static('public'));
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
