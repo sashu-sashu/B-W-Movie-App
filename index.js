572ee522e934cce7728e9709e7b20755772b17cb
Aleksandra Tiede <105430184+sashu-sashu@users.noreply.github.com>
2022-08-01 17:04:57 +0200

    Backend API (#1)
    * created movies json file
    
    * get, use, post, delete methods added to user and to movie list
    
    * added info to the documentation file
    
    * added info to documentation file
    
    * express code to route endpointsadded

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const movieList = require('./public/movie-list.json');
+const uuid = require('uuid');
 const app = express();
 
 // error handler
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-
+/*
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}));*/
+
+let users = [
+  {
+    id: 1,
+    name: "Gretchen",
+    favoriteMovies: ["Psycho"]
+  },
+  {
+    id: 2,
+    name: "Ada",
+    favoriteMovies: []
+  }
+]  
 
 app.use(bodyParser.json());
 app.use(methodOverride());
@@ -21,81 +35,141 @@ app.use((err, req, res, next) => {
 
 app.use(morgan('common'));
 
+// CREATE new user
+app.post('/users', (req, res) => {
+  const newUser = req.body;
 
-let topMovies = [
-    {
-      title: 'Coffee and Cigarettes',
-      director: 'Jim Jarmusch',
-      year: '2003',
-      duration: '96 min'
-    },
-    {
-      title: 'Schindler/’s List',
-      director: 'J.R.R. Tolkien',
-      year: '1993',
-      duration: '195 min'
-    },
-    {
-      title: 'Metropolis',
-      director: 'Fritz Lang',
-      year: '1926',
-      duration: '109 min'
-    },
-    {
-      title: 'Psycho',
-      director: 'Alfred Hitchcock',
-      year: '1960',
-      duration: '109 min'
-    },
-    {
-      title: 'To Kill A Mockingbird',
-      director: 'Robert Mulligan',
-      year: '1962',
-      duration: '129 min'
-    },
-    {
-      title: 'All About Eve',
-      director: 'Joseph L. Mankiewicz',
-      year: '1950',
-      duration: '138 min'
-    },
-    {
-      title: 'Casablanca',
-      director: 'Starring Humphrey Bogart and Ingrid Bergman',
-      year: '1942',
-      duration: '102 min'
-    },
-    {
-      title: 'The Seventh Seal',
-      director: 'Ingmar Bergman',
-      year: '1957',
-      duration: '2 h'
-    },
-    {
-      title: 'Modern Times',
-      director: 'Charles Chaplin',
-      year: '1936',
-      duration: '87 min'
-    },
-    {
-      title: '8½',
-      director: 'Federico Fellini',
-      year: '1963',
-      duration: '138 min'
-    },
-    
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send('Users need names!');
+  }
+});
 
-  ];
+// UPDATE user name
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find( user => user.id == id ); //search user by id
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('No such user found!');
+  }
+});
+
+// Remove user
+app.delete('/users/:id/', (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find( user => user.id == id ); //search user by id
+
+  if (user) {
+    users= users.filter( user => user.id != id);
+    res.json(users)
+    //res.status(200).send(`${user.name}'s account has been deleted!`);
+  } else {
+    res.status(400).send('No such user found!');
+  }
+});
+
 
 // GET requests
 app.get('/', (req, res) => {
-  let responseText = 'Welcome to my Movies app!';
+  let responseText = 'My Movies';
   responseText += '<small>Requested at: ' + req.requestTime + '</small>';
   res.send(responseText);
 });
 
-app.get('/movies', (req, res) => {                  
-  res.sendFile('public/movies.html', { root: __dirname });
+app.get('/movie-list', (req, res) => {                  
+  res.sendFile('public/movie-list.html', { root: __dirname });
+});
+
+// movies API
+app.get('/movieList', (req, res)=> {
+  res.json(movieList);
+});
+
+app.get('/movieList/:title', (req, res) => {
+  const {title} = req.params;
+  const movie = movieList.find( movie => movie.title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('Movie not found');
+  }
+});
+
+app.get('/movieList/genre/:genreName', (req, res) => {
+  const {genreName} = req.params;
+  const genre = movieList.find( movie => movie.genre === genreName).genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send('Genre not found');
+  }
+});
+
+app.get('/movieList/directors/:directorName', (req, res) => {
+  const {directorName} = req.params;
+  const director = movieList.find( movie => movie.director.name === directorName).director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send('Director not found');
+  }
+});
+
+//CREATE
+app.post('/api/movieList', (req, res) => {
+  let newMovie = req.body;
+
+  if (!newMovie.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    console.log(newMovie)
+    newMovie.id = uuid.v4();
+    movieList.push(newMovie);
+    res.status(201).send(newMovie);
+  }
+});
+
+// Create. Add new movie to favorite movies array
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find( user => user.id == id ); //search user by id
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    /*res.status(200).json(user);*/ //shows added movie in array
+    res.status(200).send(`${movieTitle} has been added to ${user.name}'s array`);
+  } else {
+    res.status(400).send('No such user found!');
+  }
+});
+
+// Remove movie from favorite movies array
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find( user => user.id == id ); //search user by id
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+    res.status(200).send(`${movieTitle} has been Removed from ${user.name}'s array`);
+  } else {
+    res.status(400).send('No such user found!');
+  }
 });
 
 app.get('/documentation', (req, res) => {                  
@@ -105,6 +179,12 @@ app.get('/documentation', (req, res) => {
 // function for static files
 // this syntax does not work
 //app.use('/documentation', express.static('public'));
+
+// error handling code
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
 // listen for requests
 app.listen(8080, () => {
