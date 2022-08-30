d3710e1760215070823c99409fb7609bd11cc65e
sashu-sashu <iamsashaalexandra@gmail.com>
2022-08-30 22:30:47 +0200

    auth comment again

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
 // READ
 //return JSON obj when at /movies
 app.get('/movies', 
-//('jwt', { session: false }), 
+('jwt', { session: false }), 
 (req, res) => {
   movies.find()
   .then((movies) => {
