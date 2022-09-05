9cd7c2d6aaad12699e09d418ad42601de16cc687
sashu-sashu <iamsashaalexandra@gmail.com>
2022-09-05 11:59:31 +0200

    better cors setup

--- index.js
+++ index.js
@@ -23,11 +23,22 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
   flags: 'a',
 });
 
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.use(morgan('combined', { stream: accessLogStream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const cors = require('cors');
+
 const { check, validationResult } = require('express-validator');
 
 app.use(cors());
